f6444e7000d6133c54bd2419e2005ee11fee37da
ManuelLopezCriquet <manulc@hotmail.com.ar>
2024-08-25 02:44:12 -0300

    Agrego Interfaz

---
+++ calculator/main.py
@@ -0,0 +1,6 @@
+from src import interfaz
+
+numero1 = float(input("Ingrese un numero:  "))
+print("Ingrese una de las siguientes operaciones: ")
+operacion = input("|  +  |  -  |  /  |  *  |\nIngresa tu operación: ")
+interfaz.opciones(operacion)
