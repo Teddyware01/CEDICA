b56d4692489511ddb7d60d57a9e973e16d40e91b
ManuelLopezCriquet <manulc@hotmail.com.ar>
2024-08-25 03:15:14 -0300

    Integracion de la  funcionalidad sumar

--- calculator/main.py
+++ calculator/main.py
@@ -3,5 +3,6 @@ from src import interfaz
 numero1 = float(input("Ingrese un numero:  "))
 print("Ingrese una de las siguientes operaciones: ")
 operacion = input("|  +  |  -  |  /  |  *  |\nIngresa tu operación: ")
-interfaz.opciones(operacion)
+resultado = interfaz.opciones(operacion,numero1)
+print(F"El resultado de la operacion es: {resultado}")
 
--- calculator/src/interfaz.py
+++ calculator/src/interfaz.py
@@ -1,14 +1,18 @@
 from src import sumar
 
-def opciones(op):
+def opciones(op, num1):
     match op:
         case '+':
-            return sumar.sumar
+            numero2 = float(input("Ingrese otro numero:  "))
+            return sumar.sumar(num1,numero2)
         case '-':
+            numero2 = float(input("Ingrese otro numero:  "))
             print("algo")
         case '/':
+            numero2 = float(input("Ingrese otro numero:  "))
             print("algo")
         case '*':
+            numero2 = float(input("Ingrese otro numero:  "))
             print("algo") 
         case _:
             print("Operacion invalida ")
