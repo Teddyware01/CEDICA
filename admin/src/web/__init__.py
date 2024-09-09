6bc5947ad8a4a7da144e6b2cf31cb5fa03cfb0fa
ManuelLopezCriquet <manulc@hotmail.com.ar>
2024-09-09 20:45:24 -0300

    Manejo de errorres 404,500

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-
+from src.web.handlers import error
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
@@ -13,5 +13,10 @@ def create_app(env="development", static_folder="../../static"):
     def sobre_nosotros():
         return render_template("sobre_nosotros.html")
     
+   
+
+    app.register_error_handler(404, error.error_not_found)
+    app.register_error_handler(500, error.error_internal_server_error)
+
     return app
 
