786442f4c91150c440d36b2259c8bbce32d8fb2a
Martin dos Santos <martunahuel@hotmail.com>
2024-09-26 17:54:03 -0300

    registro blueprint EQUIPO en la app

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from src.web.handlers import error
-
+from src.web.controllers.equipo import equipo_blueprint
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
@@ -16,5 +16,5 @@ def create_app(env="development", static_folder="../../static"):
 
     app.register_error_handler(404, error.error_not_found)
     app.register_error_handler(500, error.error_internal_server_error)
-
+    app.register_blueprint(equipo_blueprint)
     return app
