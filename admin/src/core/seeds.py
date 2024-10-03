be0904f5e4e68d52636250692df303f8d2d26440
Martin dos Santos <martunahuel@hotmail.com>
2024-10-03 18:05:36 -0300

    agrego provincias y formularios al seeds, codigo separado en archivos SQL.

--- admin/src/core/seeds.py
+++ admin/src/core/seeds.py
@@ -1,6 +1,18 @@
 from src.core import board
 from src.core import auth
 
+from src.core import equipo
+from src.core.equipo.extra_models import Provincia 
+
+from src.core.database import db
+from sqlalchemy import text
+def ejecutar_sql_script(file_path):
+    with open(file_path, 'r',encoding='utf-8') as sql_file:
+        sql_script = sql_file.read()
+    
+    with db.engine.connect() as connection:
+        connection.execute(text(sql_script))
+        connection.commit()
 
 def run():
     issue1 = board.create_issue(
@@ -81,3 +93,39 @@ def run():
     )
     board.assign_labels(issue1, [label1])
     board.assign_labels(issue2, [label1, label2])
+
+    # Profesiones
+    equipo.add_profesion(nombre="Psicologo/a")
+    equipo.add_profesion(nombre="Psicomotricista")
+    equipo.add_profesion(nombre="Medico/a")
+    equipo.add_profesion(nombre="Kinesiologo/a")
+    equipo.add_profesion(nombre="Terapista Ocupacional")
+    equipo.add_profesion(nombre="Psicopedagogo/a")
+    equipo.add_profesion(nombre="Docente")
+    equipo.add_profesion(nombre="Profesor/a")
+    equipo.add_profesion(nombre="Fonoaudiólogo/a")
+    equipo.add_profesion(nombre="Veterinario")
+    equipo.add_profesion(nombre="Otro")
+
+    # Puestos laborales
+    equipo.add_puesto_laboral(nombre="Administrativo/a")
+    equipo.add_puesto_laboral(nombre="Terapeuta")
+    equipo.add_puesto_laboral(nombre="Conductor")
+    equipo.add_puesto_laboral(nombre="Auxiliar de pista")
+    equipo.add_puesto_laboral(nombre=" Herrero")
+    equipo.add_puesto_laboral(nombre="Entrenador de Caballos")
+    equipo.add_puesto_laboral(nombre="Domador")
+    equipo.add_puesto_laboral(nombre="Profesor de Equitación")
+    equipo.add_puesto_laboral(nombre="Docente de Capacitación")
+    equipo.add_puesto_laboral(nombre="Auxiliar de mantenimiento")
+    equipo.add_puesto_laboral(nombre="Otro")
+    
+    
+    # Provincias
+    
+    ejecutar_sql_script('src\core\sql\insert_provincias.sql')
+    # Localidades
+    ejecutar_sql_script('src\core\sql\insert_localidades.sql')
+    
+    
+    
