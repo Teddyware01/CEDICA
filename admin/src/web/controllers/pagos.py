32e8563fcd122e38edf08ae8686b9e8cef3ddc38
FrancoViscardi23 <franviscardi@hotmail.com>
2024-10-20 02:10:57 -0300

    agrego cambios en consultas

--- admin/src/web/controllers/pagos.py
+++ admin/src/web/controllers/pagos.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, request, render_template, redirect, url_for, flash 
+from flask import Blueprint, request, render_template, redirect, url_for, flash
 from src.core.database import db
 from src.core.pagos.models import Pago as Pagos
 from src.core.equipo.models import Empleado
 from src.core.pagos.forms import PagoForm
+from src.core import pagos
 from src.web.handlers.auth import login_required, check
 
 pagos_bp = Blueprint("pagos", __name__, template_folder="../templates/pagos")
+
 
 @pagos_bp.route("/registrar", methods=["GET", "POST"])
 @login_required
@@ -14,8 +16,8 @@ def registrar_pago():
     form = PagoForm()
 
     if form.validate_on_submit():
-        if form.tipo_pago.data == 'honorario' and form.beneficiario.data:
-            empleado = Empleado.query.get(form.beneficiario.data)
+        if form.tipo_pago.data == "honorario" and form.beneficiario.data:
+            empleado = pagos.obtener_empleado(form)
             beneficiario = f"{empleado.nombre} {empleado.apellido}"
         else:
             beneficiario = form.otro_beneficiario.data
@@ -28,12 +30,14 @@ def registrar_pago():
             descripcion=form.descripcion.data,
         )
 
-        db.session.add(nuevo_pago)
-        db.session.commit()
+        pagos.agregar_empleado(nuevo_pago)
 
-        return redirect(url_for('pagos.listar_pagos', success="Pago registrado exitosamente."))
+        return redirect(
+            url_for("pagos.listar_pagos", success="Pago registrado exitosamente.")
+        )
 
     return render_template("registrar_pago.html", form=form)
+
 
 @pagos_bp.route("/listado", methods=["GET"])
 @login_required
@@ -41,40 +45,47 @@ def registrar_pago():
 def listar_pagos():
     orden = request.args.get("orden", "asc")
     success = request.args.get("success")
-    pagos_realizado = Pagos.query.order_by(Pagos.fecha_pago.asc() if orden == "asc" else Pagos.fecha_pago.desc()).all()
-    return render_template("listado_pagos.html", pagos_realizado=pagos_realizado, orden=orden, success=success)
+    pagos_realizado = pagos.ordenar_pagos(orden)
+    return render_template(
+        "listado_pagos.html",
+        pagos_realizado=pagos_realizado,
+        orden=orden,
+        success=success,
+    )
+
 
 @pagos_bp.route("/eliminar/<int:id>", methods=["POST"])
 @login_required
 @check("pago_destroy")
 def eliminar_pago(id):
-    pago = Pagos.query.get_or_404(id)
-    db.session.delete(pago)
-    db.session.commit()
+    pago = pagos.obtener_pago(id)
+    pagos.eliminar_pago(pago)
     return redirect(url_for("pagos.listar_pagos"))
+
 
 @pagos_bp.route("/<int:id>", methods=["GET"])
 @login_required
 @check("pago_show")
 def mostrar_pagos(id):
-    pago = Pagos.query.get_or_404(id)
+    pago = pagos.obtener_pago(id)
     return render_template("show_pago.html", pago=pago)
+
 
 @pagos_bp.route("/editar/<int:id>", methods=["GET", "POST"])
 @login_required
 @check("pago_update")
 def editar_pago(id):
-    pago = Pagos.query.get_or_404(id)
-    
-    if pago.tipo_pago != 'honorario':
+    pago = pagos.obtener_pago(id)
+
+    if pago.tipo_pago != "honorario":
         form = PagoForm(obj=pago, otro_beneficiario=pago.beneficiario)
     else:
         form = PagoForm(obj=pago)
 
     if form.validate_on_submit():
-        if form.tipo_pago.data == 'honorario':
+        if form.tipo_pago.data == "honorario":
             if form.beneficiario.data:
-                empleado = Empleado.query.get(form.beneficiario.data)
+                empleado = pagos.obtener_empleado(form)
                 pago.beneficiario = f"{empleado.nombre} {empleado.apellido}"
         else:
             pago.beneficiario = form.otro_beneficiario.data
@@ -84,27 +95,32 @@ def editar_pago(id):
         pago.tipo_pago = form.tipo_pago.data.lower()
         pago.descripcion = form.descripcion.data
 
-        db.session.commit()
+        pagos.editar_pago_db()
 
-        return redirect(url_for('pagos.listar_pagos', success="Pago editado exitosamente."))
+        return redirect(
+            url_for("pagos.listar_pagos", success="Pago editado exitosamente.")
+        )
 
     return render_template("editar_pago.html", form=form, pago=pago)
+
 
 @pagos_bp.route("/search", methods=["GET"])
 @login_required
 @check("pago_index")
 def buscar_pagos():
-    tipo_pago = request.args.get("tipo_pago").lower() if request.args.get("tipo_pago") else None
+    tipo_pago = (
+        request.args.get("tipo_pago").lower() if request.args.get("tipo_pago") else None
+    )
     fecha_inicio = request.args.get("fecha_inicio")
     fecha_fin = request.args.get("fecha_fin")
     orden = request.args.get("orden", "asc")
-    
-    query = Pagos.query
-    if tipo_pago:
-        query = query.filter(db.func.lower(Pagos.tipo_pago) == tipo_pago)
-    if fecha_inicio and fecha_fin:
-        query = query.filter(Pagos.fecha_pago.between(fecha_inicio, fecha_fin))
-    
-    pagos_realizado = query.order_by(Pagos.fecha_pago.asc() if orden == "asc" else Pagos.fecha_pago.desc()).all()
 
-    return render_template("listado_pagos.html", pagos_realizado=pagos_realizado, orden=orden)
+    pagos_realizado = pagos.buscar_pagos(tipo_pago, fecha_inicio, fecha_fin)
+
+    pagos_realizado = pagos_realizado.order_by(
+        Pagos.fecha_pago.asc() if orden == "asc" else Pagos.fecha_pago.desc()
+    ).all()
+
+    return render_template(
+        "listado_pagos.html", pagos_realizado=pagos_realizado, orden=orden
+    )
