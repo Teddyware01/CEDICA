0be3b219947479283a5d5e277663c307fce4878c
Martin dos Santos <martunahuel@hotmail.com>
2024-11-19 09:29:58 -0300

    Agrego funcionamiento con variables de entorno en request a la API

--- portal/src/stores/issues.js
+++ portal/src/stores/issues.js
@@ -1,23 +1,26 @@
-import {defineStore} from "pinia"
-import  axios from "axios"
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+
 export const useIssues = defineStore('issuesStore', {
-    state:() => ({
-        issues: [],
-        loading: false,
-        error: null,
-    }),
-    actions:{
-        async fetchIssues(){
-            try {
-                this.loading=true
-                this.error=null
-                const response = await axios.get("http://localhost:5000/api/consultas")  //cambiar esto para que deje de estar hardcodead (separar dev de deploy):
-                this.issues = response.data
-            } catch (error){
-                this.error = "Error al obtener los issues"
-            } finally {
-                this.loading = false
-            }
-        }
+  state: () => ({
+    issues: [],
+    loading: false,
+    error: null,
+  }),
+  actions: {
+    async fetchIssues() {
+      try {
+        this.loading = true;
+        this.error = null;
+        const baseUrl = import.meta.env.VITE_FLASK_API_URL;
+        const response = await axios.get(`${baseUrl}/api/consultas`);
+        this.issues = response.data;
+      } catch (error) {
+        this.error = "Error al obtener los issues";
+      } finally {
+        this.loading = false;
+      }
     }
-})
+  }
+});
--- portal/src/stores/noticias.js
+++ portal/src/stores/noticias.js
@@ -11,7 +11,8 @@ export const useNoticias = defineStore('noticiasStore', {
             try {
                 this.loading=true
                 this.error=null
-                const response = await axios.get("http://localhost:5000/api/noticias")  //cambiar esto para que deje de estar hardcodead (separar dev de deploy):
+                const baseUrl = import.meta.env.VITE_FLASK_API_URL;
+                const response = await axios.get(`${baseUrl}/api/noticias`);
                 this.noticia = response.data
             } catch (error){
                 this.error = "Error al obtener las noticias"
