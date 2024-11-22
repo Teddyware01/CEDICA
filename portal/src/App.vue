e79976e2841c72fede6d358dbe99b006f4ec309a
Martin dos Santos <martunahuel@hotmail.com>
2024-11-22 10:36:49 -0300

    Agrego Icono a la app. (hacer NPM INSTALL)

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -9,4 +9,11 @@
 <script setup>
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import { useHead } from '@vueuse/head';
+
+  useHead({
+    link: [
+      { rel: 'icon', href: '/cedica_icon.png' },
+    ],
+  });
 </script>
